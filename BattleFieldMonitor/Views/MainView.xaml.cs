e5c70b16e9634f5ea3da2679b287319ff5a9c2ab
Shishkov Aleksey <ndaleksey@yandex.ru>
2017-05-18 15:01:13 +0300

    Добавил проект с картой

---
+++ BattleFieldMonitor/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Swsu.BattleFieldMonitor.Views
+{
+	/// <summary>
+	/// Логика взаимодействия для MainView.xaml
+	/// </summary>
+	public partial class MainView : Window
+	{
+		public MainView()
+		{
+			InitializeComponent();
+		}
+	}
+}
