7a080bdf96f39f00712950fb29aa525c672d6d6a
borodin <borodin_mikhail@mail.ru>
2017-05-30 13:38:24 +0300

    В класс Database приходят извещения об изменениях в таблицах. Более пока ничего не делается.

---
+++ BattleFieldMonitor/Services/Implementations/Notifications/Notification.cs
@@ -0,0 +1,56 @@
+﻿using Swsu.Common;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Swsu.BattleFieldMonitor.Services.Implementations.Notifications
+{
+    [XmlRoot("notification")]
+    public class Notification
+    {
+        #region Fields
+        private static XmlSerializer _serializer;
+        #endregion
+
+        #region Properties
+        [XmlElement("new")]
+        public PrimaryKey New
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("old")]
+        public PrimaryKey Old
+        {
+            get;
+            set;
+        }
+
+        [XmlAttribute("op")]
+        public Operation Operation
+        {
+            get;
+            set;
+        }
+
+        [XmlAttribute("tableName")]
+        public string TableName
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Methods        
+        public static Notification Parse(string source)
+        {
+            var serializer = PropertyHelpers.Initialized(ref _serializer, () => new XmlSerializer(typeof(Notification)));
+
+            using (var reader = new StringReader(source))
+            {
+                return (Notification)serializer.Deserialize(reader);
+            }
+        }
+        #endregion
+    }
+}
---
+++ BattleFieldMonitor/Services/Implementations/Notifications/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace Swsu.BattleFieldMonitor.Services.Implementations.Notifications
+{
+    public enum Operation
+    {
+        [XmlEnum("INSERT")]
+        Insert,
+
+        [XmlEnum("UPDATE")]
+        Update,
+
+        [XmlEnum("DELETE")]
+        Delete
+    }
+}
