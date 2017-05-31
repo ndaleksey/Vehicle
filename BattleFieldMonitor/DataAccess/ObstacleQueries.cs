18d61534e36072e44c8450e88a624b5856c40cd4
Shishkov Aleksey <ndaleksey@yandex.ru>
2017-05-31 19:00:04 +0300

    Добавил интерфейс и класс препятствия

---
+++ BattleFieldMonitor/DataAccess/ObstacleQueries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using Swsu.BattleFieldMonitor.Models.Implementations;
+
+namespace Swsu.BattleFieldMonitor.DataAccess
+{
+	internal static class ObstacleQueries
+	{
+		#region Methods
+		/*public static IEnumerable<> SelectAll(DbConnection connection)
+		{
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "SELECT id, display_name, x, y, heading, speed FROM nkb_vs.unmanned_vehicle";
+
+				foreach (var r in GetResults(command))
+				{
+					yield return r;
+				}
+			}
+		}*/
+		
+
+		#endregion
+	}
+}
---
+++ BattleFieldMonitor/Services/Implementations/Repositories/ObstacleRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Common;
+using Swsu.BattleFieldMonitor.Models;
+using Swsu.BattleFieldMonitor.Models.Implementations;
+
+namespace Swsu.BattleFieldMonitor.Services.Implementations.Repositories
+{
+	internal class ObstacleRepository : Repository<Obstacle, IObstacle>
+	{
+		protected override void LoadDeltaCore(DbConnection connection, RepositoryDelta repositoryDelta)
+		{
+			
+		}
+
+		protected internal override void Reload(DbConnection connection)
+		{
+			
+		}
+	}
+}
